318d00c4910a14043b1231a9b65e5316bde19e73
Victor Usoro <victorusoro@hotmail.com>
2018-11-23 11:36:39 +0100

    updates, dependency injection added

--- MW.API/Startup.cs
+++ MW.API/Startup.cs
@@ -7,8 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MW.Application;
+using MW.Services;
 
 namespace MW.API
 {
@@ -24,13 +27,30 @@ namespace MW.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MiddleWareDBContext>(options => 
+             options.UseNpgsql(Configuration.GetConnectionString("MiddleWareConnect")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IServiceInit, ServiceInit>();
+            services.AddScoped<IMappingHandlers, MappingHandlers>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
             });
-
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = new[]
+                {
+                "application/xml",
+                "application/json",
+                "text/json",
+                "image/svg+xml"
+            };
+            options.EnableForHttps = true;
+         });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -51,7 +71,20 @@ namespace MW.API
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add(
+                    "Content-Security-Policy",
+                "img-src 'host'" +
+                "connect-src 'none'" +
+                "object-src 'none'" +
+                "media-src 'none'" +
+                "form-action 'self'" +
+                "default-src 'self'");
+                await next();
+            });
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
